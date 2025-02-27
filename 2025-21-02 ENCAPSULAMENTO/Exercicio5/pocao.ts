ae5b0791119a1a64437f6f49c9febd799a435a3c
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-02-27 16:56:52 -0300

    Update pocao.ts

--- 2025-21-02 ENCAPSULAMENTO/Exercicio5/pocao.ts
+++ 2025-21-02 ENCAPSULAMENTO/Exercicio5/pocao.ts
@@ -2,13 +2,20 @@ import { Ingrediente } from "./ingrediente";
 
 export class Pocao {
     private nome!:string;
-    private ingredientes: {nome:string, quantidade:number, efeito:string}[] = [];
+    private ingredientes: Ingrediente[] = [];
     private efeito: string[] = [];
 
     // Set
-    setNome(nomeRecebido:string) {
+    setNome(nomeRecebido:string):void {
         this.nome = nomeRecebido;
         console.log('\nNome da pocao definido!');
+    };
+    addIngrediente(...ingredienteRecebido:Ingrediente[]):void { // ... permite aceitar ilimitadas quantidades de argumentos
+        for (let i:number=0; i < ingredienteRecebido.length; i++) {
+            this.ingredientes.push(ingredienteRecebido[i]);
+            this.efeito.push(ingredienteRecebido[i].getEfeito())
+            console.log(`Ingrediente ${ingredienteRecebido[i].getNome()} adicionado!`);
+        } 
     };
 
     // Get
@@ -17,42 +24,34 @@ export class Pocao {
         console.table(this.ingredientes);
     };
 
-    addIngrediente(ingredienteRecebido:Ingrediente) {
-        let ingredienteRecebidoInfo: {nome:string, quantidade:number, efeito:string} = {
-            nome: ingredienteRecebido.getNome(),
-            quantidade: ingredienteRecebido.getQuantidade(),
-            efeito: ingredienteRecebido.getEfeito()
-        };
-
-        this.efeito.push(ingredienteRecebidoInfo.efeito);
-        this.ingredientes.push(ingredienteRecebidoInfo);
-        console.log(`Ingrediente ${ingredienteRecebidoInfo.nome} adicionado!`);
-    };
-
+    // Main
     prepararPocao():void {
         console.log(`Preparando ${this.nome}... \nPocao criada! Os seguintes efeitos foram aplicados a ela:`);
-        for (let i = 0; i < this.efeito.length; i++) {
+        for (let i:number=0; i < this.efeito.length; i++) {
             console.log(this.efeito[i])
         };
     };
 };
 
 // Criacao de ingredientes
-let ingrediente1 = new Ingrediente;
+let ingrediente1 = new Ingrediente();
 ingrediente1.setNome('Lagrima de Grifo');
 ingrediente1.setQuantidade(2);
 ingrediente1.setEfeito('Rejuvenescimento');
 
-let ingrediente2 = new Ingrediente;
+let ingrediente2 = new Ingrediente();
 ingrediente2.setNome('Coracao de Dragao');
 ingrediente2.setQuantidade(1);
 ingrediente2.setEfeito('Aumento extremo de forca');
 
+let ingrediente3 = new Ingrediente();
+ingrediente3.setNome('Pena de Coruja');
+ingrediente3.setQuantidade(8);
+ingrediente3.setEfeito('Visao noturna');
+
 // Criacao da pocao
-let pocao = new Pocao;
+let pocao = new Pocao();
 pocao.setNome('Pocao de Renascimento');
-pocao.addIngrediente(ingrediente1);
-pocao.addIngrediente(ingrediente2);
+pocao.addIngrediente(ingrediente1, ingrediente2, ingrediente3);
 pocao.getInfo();
 pocao.prepararPocao();
-
