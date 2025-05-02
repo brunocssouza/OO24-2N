e949441c6c7e4e2c9cdd6dd32f1bfdbf79354ec9
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-05-02 04:59:46 -0300

    Update CriaturaMagicaGuardiao.ts

--- 2025-24-04 AVALIACAO 1 (Parte Final)/CriaturaMagicaGuardiao.ts
+++ 2025-24-04 AVALIACAO 1 (Parte Final)/CriaturaMagicaGuardiao.ts
@@ -3,16 +3,11 @@ import { CriaturaMagica } from "./CriaturaMagica";
 export abstract class Guardiao extends CriaturaMagica {
     protected dano:number
     protected segredo:string
-    protected static guardioes:Guardiao[] = []
 
     constructor(nomeRecebido:string, hpRecebido:number, tipoRecebido:string, poderRecebido:string, danoRecebido:number, segredoRecebido:string) {
         super(nomeRecebido, hpRecebido, tipoRecebido, poderRecebido)
         this.dano = danoRecebido
         this.segredo = segredoRecebido
-    }
-
-    static getAll():Guardiao[] {
-        return this.guardioes
     }
 
     abstract protegerSegredo():void
